d074108a7ffe7e6ed50cbd3a89abfa9b902f6736
Bignasca Roberto, INI-DOS-OCD <Roberto.Bignasca@swisscom.com>
2022-05-09 14:44:55 +0200

    Spring inizializer

---
+++ src/main/java/com/swisscom/DataAPIChallenge/DataApiChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.swisscom.DataAPIChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataApiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataApiChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swisscom/DataAPIChallenge/DataApiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swisscom.DataAPIChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataApiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
